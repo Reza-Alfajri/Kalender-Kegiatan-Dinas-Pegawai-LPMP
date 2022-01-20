df49dfdc51bc4aae476e03f6b5d3833aefc75a68
Sahdiki <79701880+sahdikigg@users.noreply.github.com>
2022-01-20 14:08:21 +0700

    baru gas

--- index.php
+++ index.php
@@ -1,49 +1,65 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="style.css">
-</head>
-<body>
-<div class="slideshow-container">
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<div class="mySlides fade">
-    <iframe style="border: solid 1px #777;" src="https://calendar.google.com/calendar/embed?height=800&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=Asia%2FJakarta&amp;src=Y185NjIyYWwzZDJhNTVpMm9uOWZ1b2Z0dThoa0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&amp;src=Y19iYm1yMjVodWhocWszY2szNDhkMWc0MzhqOEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&amp;src=Y19zZmplNG4yYW1yczIxdTU5YmtkbnJhbXVwc0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&amp;src=Y192YXEybDFuOGM5bGNwNTV0bGt2cXFiYTNlNEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%23D50000&amp;color=%230B8043&amp;color=%238E24AA&amp;color=%23A79B8E&amp;title=Jadwal%20Kegiatan%20LPMP&amp;showTabs=1&amp;showTz=0&amp;showNav=1" width="1120" height="600" frameborder="0" scrolling="no"></iframe>
-</div>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- Style -->
+    <link rel="stylesheet" href="css/style.css">
+    <title>Kegiatan Dinas Pegawai | LPMP Kalbar</title>
+  </head>
+  <body>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="rgb(24, 171, 216)" fill-opacity="1" d="M0,224L120,197.3C240,171,480,117,720,112C960,107,1200,149,1320,170.7L1440,192L1440,0L1320,0C1200,0,960,0,720,0C480,0,240,0,120,0L0,0Z"></path></svg>
+    <section class="calendar" id="calendar">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card border-0 d-flex justify-content-center shadow mt-0 p-5 pb-3 pt-4">
+                        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+                            <div class="carousel-inner">
+                                <div class="carousel-item active" data-bs-interval="150000">
+                                    <iframe src="https://calendar.google.com/calendar/embed?height=800&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=Asia%2FJakarta&amp;src=Y185NjIyYWwzZDJhNTVpMm9uOWZ1b2Z0dThoa0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&amp;src=Y19iYm1yMjVodWhocWszY2szNDhkMWc0MzhqOEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&amp;src=Y19zZmplNG4yYW1yczIxdTU5YmtkbnJhbXVwc0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&amp;src=Y192YXEybDFuOGM5bGNwNTV0bGt2cXFiYTNlNEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%23D50000&amp;color=%230B8043&amp;color=%238E24AA&amp;color=%23A79B8E&amp;title=Jadwal%20Kegiatan%20LPMP&amp;showTabs=1&amp;showTz=0&amp;showNav=1" width="100%" height="445" frameborder="0" scrolling="yes"></iframe>
+                                </div>
+                                <div class="carousel-item" data-bs-interval="150000">
+                                    <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vT5mQOvVzV6iRNL5rm6xrSf3mrrVcUP9WwbNAdI3wyijsAkMQeQe4nwy48jGI_AGkAg0JKpuwZ4RVet/pubhtml" width="100%" height="445" frameborder="none" scrolling="yes"></iframe>
+                                </div>
+                            </div>
+                            <button class="carousel-control-prev-icon border-0 p-0 bg-secondary prev rounded" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev"></button>
+                            <button class="carousel-control-next-icon border-0 p-0 bg-secondary next rounded" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next"></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <div class="card head text-center shadow-sm d-flex flex-row">
+                        <img src="img/logo-lpmp-kecil.png" class="mt-0 mb-1 ms-3" width="50px" alt="">
+                        <h6 class=" fw-bold shadow-0 mt-2">Kalender Kegiatan Dinas Pegawai LPMP Kalimantan Barat</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer class="">
+        <div class="content">
+            <div class="col-12 text-center p-5 pb-0 mb-5">
+                <hr>
+                <span class="text-muted">&copy; 2022 Lembaga Penjaminan Mutu Pendidikan Kalimantan Barat | Made by Polnep Student Interns</span>
+            </div>
+        </div>
+        
+    </footer>
+    <!-- Optional JavaScript; choose one of the two! -->
 
-<div class="mySlides fade">
-    <iframe style="border: solid 1px #777;" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vT5mQOvVzV6iRNL5rm6xrSf3mrrVcUP9WwbNAdI3wyijsAkMQeQe4nwy48jGI_AGkAg0JKpuwZ4RVet/pubhtml" width="1120" height="600" frameborder="0" scrolling="no"></iframe>
-</div>
-
-</div>
-<br>
-
-<div style="text-align:center; visibility: hidden;">
-  <span class="dot"></span> 
-  <span class="dot"></span> 
-</div>
-
-<script>
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 300000); // Change image every 2 seconds
-}
-</script>
-
-</body>
-</html> 
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+<!--     
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+     -->
+  </body>
+</html>
